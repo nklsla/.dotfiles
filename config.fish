bf81a4ef8a5540fed079289a6b8d47a17c247bb7
Niklas Larsson <niklaslarsson500@hotmail.com>
2026-01-17 22:14:06 +0100

    Update fish config with ssh-agent

--- config.fish
+++ config.fish
@@ -6,3 +6,23 @@ end
 set -Ux EDITOR vim
 set -Ux VISUAL vim
 
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c) >/dev/null
+end
+
+if not set -q SSH_AUTH_SOCK
+    eval (ssh-agent -c)
+end
+
+for key in ~/.ssh/*
+    if test -f $key
+        if not string match -q '*.pub' $key
+            if not string match -q '*known_hosts*' $key
+                if not string match -q '*config*' $key
+                    ssh-add $key >/dev/null 2>&1
+                end
+            end
+        end
+    end
+end
+
